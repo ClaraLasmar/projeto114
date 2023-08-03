32e0309bca33c382cdf86796e40149220a30a05c
ClaraLasmar <114614612+ClaraLasmar@users.noreply.github.com>
2023-08-03 09:34:39 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
 function preload()  {
-
 }
 
 function setup() {
     canvas = createCanvas(300, 300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.lenght > 0)
+    {
+        console.log(results)
+        console.log("forehead x = " + results[0].pose.forehead.x);
+        console.log("forehead y = " + results[0].pose.forehead.y);
+    }
 }
 
 function draw() {
-
+    image(video, 0, 0, 300, 300);
 }
-function takeSnapshot(){
-    save('myFilterImage.png');
-}
+    function takeSnapshot(){
+        save('myFilterImage.png');
+    }
+    
